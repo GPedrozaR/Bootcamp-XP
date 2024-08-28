09e4fff6581267a6e63682ff276cefa9128cfebe
GPedrozaR <gpedrozarodrigues@gmail.com>
2024-08-28 11:50:20 -0300

    feat: Parking system challenge.

---
+++ ParkingManager/ParkingManager/Models/Parking.cs
@@ -0,0 +1,64 @@
+﻿namespace ParkingManager.Models
+{
+	internal class Parking
+	{
+		/// <summary>
+		/// </summary>
+		/// <param name="initialPrice">Preço inicial</param>
+		/// <param name="hourlyPrice">Preço por hora</param>
+		public Parking(decimal initialPrice, decimal hourlyPrice)
+		{
+			InitialPrice = initialPrice;
+			HourlyPrice = hourlyPrice;
+		}
+
+		private decimal InitialPrice { get; set; } = 0;
+		private decimal HourlyPrice { get; set; } = 0;
+		private List<string> Vehicles { get; set; } = [];
+
+		public void AddVehicle()
+		{
+			Console.WriteLine("Digite a placa do veículo para estacionar:");
+
+			var plate = Console.ReadLine() ?? "N/A";
+
+			Vehicles.Add(plate);
+
+			Console.WriteLine($"Veículo {plate} adicionado com sucesso.");
+		}
+
+		public void RemoveVehicle()
+		{
+			Console.WriteLine("Digite a placa do veículo para remover:");
+			var plate = Console.ReadLine() ?? "N/A";
+
+			if (!Vehicles.Exists(x => x.Equals(plate, StringComparison.OrdinalIgnoreCase)))
+				Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+			else
+			{
+				Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+
+				if (!int.TryParse(Console.ReadLine(), out var hours))
+					Console.WriteLine("Digite uma quantidade de horas válidas");
+
+				decimal value = InitialPrice + (HourlyPrice * hours);
+
+				Vehicles.Remove(plate);
+
+				Console.WriteLine($"O veículo {plate} foi removido e o preço total foi de: R$ {value}");
+			}
+		}
+
+		public void ListVehicles()
+		{
+			if (Vehicles.Count == 0)
+			{
+				Console.WriteLine("Não há veículos estacionados.");
+				return;
+			}
+
+			Vehicles.ForEach(Console.WriteLine);
+		}
+
+	}
+}
---
+++ ParkingManager/ParkingManager/Program.cs
@@ -0,0 +1,62 @@
+﻿using ParkingManager.Models;
+
+namespace ParkingManager
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+			Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+							  "Digite o preço inicial:");
+
+			var initialPrice = Convert.ToDecimal(Console.ReadLine());
+
+			Console.WriteLine("Agora digite o preço por hora:");
+
+			decimal hourlyPrice = Convert.ToDecimal(Console.ReadLine());
+
+			var park = new Parking(initialPrice, hourlyPrice);
+
+			var showMenu = true;
+			while (showMenu)
+			{
+				Console.Clear();
+				Console.WriteLine("Digite a sua opção:");
+				Console.WriteLine("1 - Cadastrar veículo");
+				Console.WriteLine("2 - Remover veículo");
+				Console.WriteLine("3 - Listar veículos");
+				Console.WriteLine("4 - Encerrar");
+
+				switch (Console.ReadLine())
+				{
+					case "1":
+						park.AddVehicle();
+						break;
+
+					case "2":
+						park.RemoveVehicle();
+						break;
+
+					case "3":
+						park.ListVehicles();
+						break;
+
+					case "4":
+						showMenu = false;
+						break;
+
+					default:
+						Console.WriteLine("Opção inválida");
+						break;
+				}
+
+				Console.WriteLine("Pressione uma tecla para continuar");
+				Console.ReadLine();
+			}
+
+			Console.WriteLine("O programa se encerrou");
+		}
+	}
+}
